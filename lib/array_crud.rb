14a0715a5a4aef4563db7f58aee49515f0cf745b
Michaylah Mam <mchylh@gmail.com>
2020-03-05 19:15:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
     array << (element[x])
     x+=1
   end
-  array
+  array.push("orange")
 end
 
 def add_element_to_start_of_array(array, element)
