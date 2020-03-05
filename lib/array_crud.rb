a77ddd796f7feb6f98387432e3b587f560a03388
Michaylah Mam <mchylh@gmail.com>
2020-03-05 19:39:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,12 +18,12 @@ end
 
 def remove_element_from_end_of_array(array)
   ["red", "blue", "green", "yellow"]
-  array.pop(3)
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
   ["red", "blue", "green", "yellow"]
-  array.shift(0)
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
